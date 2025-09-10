c7691d3f045cfdaf78119edec9e675c3c77fdff3
Noam Eichelberg <Noameich@gmail.com>
2025-09-10 14:11:51 +0300

    Add project files.

---
+++ MyFirstProjectNoam/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MyFirstProjectNoam")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MyFirstProjectNoam.Pages")]
